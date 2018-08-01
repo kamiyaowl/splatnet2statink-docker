0ec3d6d71d9f4bb38d65051452e84b0e70a23b94
K <kamiyaowl@gmail.com>
2018-08-01 19:53:56 +0900

    Fix #1

--- runner.py
+++ runner.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     skip_update = os.getenv("skip_update", "") # monitor per 3hour
     if not(skip_update):
         print("#update git submodules")
+        subprocess.call(["git", "submodule", "foreach", "git", "reset", "--hard", ])
         subprocess.call(["git", "submodule", "foreach", "git", "pull", "origin", "master", ])
     # 実行
     args = ["python", "./splatnet2statink/splatnet2statink.py"]
