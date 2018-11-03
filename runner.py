43837ce55e42307e4b9f2ebf97f6f77a512c4f78
K <fieldprogrammable@gmail.com>
2018-11-03 21:51:12 +0900

    Salmon run support

--- runner.py
+++ runner.py
@@ -8,21 +8,25 @@ def env_to_config(envs_src):
     if envs_available:
         with open('config.txt', 'w') as file:
             file.write(json.dumps(envs))
-        print('#update config.txt')
+        print('#Update config.txt')
 
 if __name__ == "__main__":
     # Docker環境変数からコンフィグを生成
     envs_src = ["api_key", "cookie", "session_token", "user_lang"]
     env_to_config(envs_src)
-    # submoduleで追加された
-    skip_update = os.getenv("skip_update", "") # monitor per 3hour
-    if not(skip_update):
-        print("#update git submodules")
+    # submoduleで追加されたsplatnet2statinkを更新
+    if not(os.getenv("skip_update", "")):
+        print("#Update git submodules")
         subprocess.call(["git", "submodule", "foreach", "git", "reset", "--hard", ])
         subprocess.call(["git", "submodule", "foreach", "git", "pull", "origin", "master", ])
     # 実行
     args = ["python", "./splatnet2statink/splatnet2statink.py"]
-    flags = os.getenv("run_flags", "-M 14400 -r").split(" ") # monitor per 4hour
+    flags = os.getenv("run_flags", "-M 14400 -r").split(" ")
     args.extend(flags)
-    print('#run {}'.format(args))
+    print('#Run {}'.format(args))
     subprocess.call(args)
+    # Salmon runを更新する
+    if not(os.getenv("skip_salmon", "")):
+        salmon_args = ["python", "./splatnet2statink/splatnet2statink.py", "--salmon"]
+        print('#Run {}'.format(salmon_args))
+        subprocess.call(salmon_args)
