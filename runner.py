e65740508e3dd2e02f15616098a285ce391fc2f7
K <kamiyaowl@gmail.com>
2018-11-12 22:42:14 +0900

    print replace yield

--- runner.py
+++ runner.py
@@ -2,10 +2,10 @@ import os
 import json
 import subprocess
 import time
-from flask import Flask
+from flask import Flask, stream_with_context, Response
 
 app = Flask(__name__)
-VERSION = "1.3.3-beta"
+VERSION = "1.3.3-beta2"
 
 @app.route('/')
 def index():
@@ -17,52 +17,54 @@ def env_to_config():
     envs = dict([(e, os.getenv(e, "")) for e in envs_src])
     envs_available = any(envs.values())
     if envs_available:
-        print('#Start Update config.txt')
+        yield ('#Start Update config.txt')
         with open('config.txt', 'w') as file:
             file.write(json.dumps(envs))
-        print('#Done Update config.txt')
+        yield ('#Done Update config.txt')
 
 def update_submodules():
-    print("#Start Update git submodules")
+    yield ("#Start Update git submodules")
     subprocess.call(["git", "submodule", "foreach", "git", "reset", "--hard", ])
     subprocess.call(["git", "submodule", "foreach", "git", "pull", "origin", "master", ])
-    print("#Done Update git submodules")
+    yield ("#Done Update git submodules")
 
 def sync_salmon():
     salmon_args = ["python", "./splatnet2statink/splatnet2statink.py", "--salmon", "-r"]
-    print('#Start Run {}'.format(salmon_args))
+    yield ('#Start Run {}'.format(salmon_args))
     p = subprocess.Popen(salmon_args, stdin=subprocess.PIPE, encoding="utf8")
     p.stdin.write("50")
     p.stdin.close()
     p.wait()
-    print('#Done Run {}'.format(salmon_args))
+    yield ('#Done Run {}'.format(salmon_args))
 
 def sync_battle():
     args = ["python", "./splatnet2statink/splatnet2statink.py"]
     flags = os.getenv("run_flags", "-r").split(" ")
     args.extend(flags)
-    print('#Start Run {}'.format(args))
+    yield ('#Start Run {}'.format(args))
     subprocess.call(args)
-    print('#Done Run {}'.format(args))
+    yield ('#Done Run {}'.format(args))
 
 @app.route('/sync')
 def sync_all():
     # 時間計測
     start = time.time()
     # Docker環境変数からコンフィグを生成
-    env_to_config()
+    for log in env_to_config():
+        yield log
     # submoduleで追加されたsplatnet2statinkを更新
     if not(os.getenv("skip_update", "")):
-        update_submodules()
+        for log in update_submodules():
+            yield log
     # 実行
-    sync_battle()
+    for log in sync_battle():
+        yield log
     # Salmon runを更新する
     if not(os.getenv("skip_salmon", "")):
-        sync_salmon()
+        for log in sync_salmon():
+            yield log
     elapsed = time.time() - start
-    result = "elapsed_time: {}".format(elapsed)
-    print(result)
-    return result
+    yield ("elapsed_time: {}".format(elapsed))
 
 if __name__ == "__main__":
     print("splatnet2statink-docker v{}".format(VERSION))
@@ -73,6 +75,7 @@ if __name__ == "__main__":
         print("REST API mode. port {} listen ...".format(run_port))
         app.run(debug=debug, host="0.0.0.0", port=run_port)
     else:
-        sync_all()
+        for log in sync_all():
+            print(log)
 
 
